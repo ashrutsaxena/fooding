b1d8a77ea4617c3fbed04349f09bce30be86fa90
ashrut saxena <ashrut.saxena_cs18@gla.ac.in>
2020-08-02 14:48:57 +0700

    foodss

---
+++ app/src/main/java/com/internshala/foodrunner/fragment/FAQFragment.kt
@@ -0,0 +1,28 @@
+package com.internshala.foodrunner.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.internshala.foodrunner.R
+import com.internshala.foodrunner.util.DrawerLocker
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FAQFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_faq, container, false)
+        (activity as DrawerLocker).setDrawerEnabled(true)
+        return view
+    }
+
+
+}
